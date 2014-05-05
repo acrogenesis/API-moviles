726614ddc17055add6f7720865c74724b262f103
Adrian Rangel <adrian.rangel@gmail.com>
2014-05-05 11:53:03 -0500

    nuevo formato de json y agregar audios

--- agua.php
+++ agua.php
@@ -2,11 +2,23 @@
   header("Content-type: application/json");
 ?>
 {
-  "videos":{
-    "1":"http://www.youtube.com/watch?v=XFOx9QnMcEg"
-  },
-  "dibujos":{
-    "nombre":"el gran tesoro",
-    "url":"http://intranet.bluehats.mx/moviles/pngs/el_gran_tesoro.png"
-  }
+    "videos": [
+        {
+            "id": "XFOx9QnMcEg",
+            "titulo": "Cuidado del agua: El gran tesoro"
+        }
+    ],
+    "dibujos": [
+        {
+            "nombre": "El gran tesoro",
+            "url": "http://intranet.bluehats.mx/moviles/pngs/el_gran_tesoro.png"
+        }
+    ],
+    "consejos": [
+        {
+            "url": "http://intranet.bluehats.mx/moviles/mp3s/agua.mp3",
+            "titulo": "Ahorrar agua",
+            "texto": "Cierra la llave después de lavarte las manos."
+        }
+    ]
 }
